7e35ee7b834c92ae44b0a8ced1e24f88fdfc1a97
enicky <enicky80@gmail.com>
2025-09-24 20:02:00 +0200

    feat: add logging for health check span filtering in tracing middleware

--- main.py
+++ main.py
@@ -45,13 +45,17 @@ class HealthzFilterSpanProcessor(SpanProcessor):
     def __init__(self, wrapped):
         self._wrapped = wrapped
     def on_start(self   , span, parent_context=None):
-
+        logger.info(f"Span started: '{span.name}'")
+        if "health" in span.name.lower():
+            logger.info("Filtered out healthz span")
         self._wrapped.on_start(span, parent_context)
 
     def on_end(self, span):
-        logger.info(f"Span ended: {span.name}")
-        if span.name == "GET /healthz http send" or span.name == "GET /healthz":
+        logger.info(f"Span ended: '{span.name}'")
+        if "health" in span.name.lower():
+            logger.info("Filtered out healthz span")
             return
+
         self._wrapped.on_end(span)
 
     def shutdown(self):
