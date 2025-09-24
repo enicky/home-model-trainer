b0783c77dc4aec0892e43f1c5163160b00d85b84
enicky <enicky80@gmail.com>
2025-09-24 20:45:38 +0200

    refactor: comment out logging statements in HealthzFilterSpanProcessor

--- Services/HealthzFilterSpanProcessor.py
+++ Services/HealthzFilterSpanProcessor.py
@@ -2,22 +2,22 @@ import logging
 
 from opentelemetry.sdk.trace import SpanProcessor
 
-logger = logging.getLogger("HealthzFilterSpanProcessor")
+#logger = logging.getLogger("HealthzFilterSpanProcessor")
 class HealthzFilterSpanProcessor(SpanProcessor):
 
     def __init__(self, wrapped):
         self._wrapped = wrapped
     def on_start(self   , span, parent_context=None):
-        logger.info(f"Span started: '{span.name}'")
+        #logger.info(f"Span started: '{span.name}'")
         if "health" in span.name.lower():
-            logger.info("Filtered out healthz span")
+            #logger.info("Filtered out healthz span")
             return
         self._wrapped.on_start(span, parent_context)
 
     def on_end(self, span):
-        logger.info(f"Span ended: '{span.name}'")
+        #logger.info(f"Span ended: '{span.name}'")
         if "health" in span.name.lower():
-            logger.info("Filtered out healthz span")
+            #logger.info("Filtered out healthz span")
             return
 
         self._wrapped.on_end(span)
